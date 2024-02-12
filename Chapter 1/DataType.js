d33bbcdaea8103f3d45cbb625af01c0eface61ec
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-02-12 10:51:04 +0600

    learn data type

--- Chapter 1/DataType.js
+++ Chapter 1/DataType.js
@@ -1,3 +1,33 @@
+//primitive
+
 // var num=10;
 // console.log(num)
 
+// var string="this is string"
+// console.log(string)
+
+// var string=`this also string 10`
+
+// console.log(string)
+
+// var something;
+// console.log(something)
+
+// var Null=null;
+// console.log(Null);
+
+
+//non-primitive
+
+// var string=10 + 100+`making number`+30;
+// console.log(string);
+
+var test=10;
+
+console.log(typeof(test));
+
+test=`10`;
+console.log(typeof(test));
+
+test=true;
+console.log(typeof(test));
