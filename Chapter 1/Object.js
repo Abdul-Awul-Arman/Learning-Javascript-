c5927e7ba9f8e7274e413ce530c47ff6b7c12bfd
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-02-15 09:59:25 +0600

    finished the learning about object

---
+++ Chapter 1/Object.js
@@ -0,0 +1,26 @@
+    // var arman= new Object ();
+
+    var arman={
+        FullName :'AbdulAwulArman',
+        age: 21,
+        address: 'sylhet',
+        job:'Developer.',
+        My_Book:["this","that","the"],
+        My_Pen:["Black","Red","White"],
+        
+    }
+
+    arman.zip=3100;
+
+    arman.job="student";
+
+    console.log(arman.My_Book[2]);
+
+
+    // arman.fun=function(){
+    //     console.log('this inside fun');
+    // };
+
+    // for(item in arman){
+    //     console.log(item);
+    // }
