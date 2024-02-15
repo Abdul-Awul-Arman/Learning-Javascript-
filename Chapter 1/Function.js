ffe823e6732099426e63dc3d55bef382d2a0f0cd
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-02-15 10:57:08 +0600

    finished the learning about function

--- Chapter 1/Function.js
+++ Chapter 1/Function.js
@@ -9,3 +9,46 @@
 // }
 
 // ThisIsFormFunction();
+
+// function fun(name,age){
+    
+//     function x(name2,age2){
+//         console.log(`${name2} ${age2}`);
+//     }
+    
+//     x(name,age);
+
+//     return x;
+// }
+
+// var rtfun=fun("arman",21);
+
+// rtfun();
+
+
+
+// console.log(rtfun);
+
+// rtfun("arman",23);e
+
+function TheMag(arman){
+    // name='x';
+    arman.name="arfat";
+    var name=arman.name;
+    console.log(`welcome Mr.`+name);
+    return function options(menu){
+        console.log(`do you like `+  menu+` mr.`+name);
+    }
+}
+
+// var name='arman';
+var Object={
+    name:"arman",
+    age:23,
+}
+
+
+
+TheMag(Object) ('coffee');
+
+console.log(Object.name);
