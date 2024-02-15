13d503fef6894839b560d49eae7be9b10d5b4aae
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-02-15 09:17:45 +0600

    learn array

---
+++ Chapter 1/array.js
@@ -0,0 +1,11 @@
+    var arry=["arman","araft","nur"];
+    // console.log(arry);
+    // var a=arry[2];
+    // console.log(a);
+
+    // console.log(arry.length);
+    // console.log(arry["length"]);
+
+    // for(var i=0;i<arry.length;i++){
+    //     console.log(arry[i]);
+    // }
