aecf71e7d5a124055c6df09902782c5cfd6869d3
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-02-15 09:34:49 +0600

    learn more about array Method.

--- Chapter 1/array.js
+++ Chapter 1/array.js
@@ -1,4 +1,4 @@
-    var arry=["arman","araft","nur"];
+    var arry=["arman","araft","nur","masrafe","somrat",""];
     // console.log(arry);
     // var a=arry[2];
     // console.log(a);
@@ -6,6 +6,21 @@
     // console.log(arry.length);
     // console.log(arry["length"]);
 
+    // arry.push("fahad");
+    // arry.pop();
+
+    // arry.unshift("masrafi");
+    // arry.shift();
+
+    
+    // console.log(arry.indexOf("araft"));
+
+
+    // var newArray= arry.splice(0,3);
+    // console.log(newArray);
+
+
+    
     // for(var i=0;i<arry.length;i++){
     //     console.log(arry[i]);
     // }
