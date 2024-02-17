140d3afc6cac5469e3182748ca9e742b874e1611
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-02-17 08:54:28 +0600

    learn more about this key word

---
+++ Chapter2/this.js
@@ -0,0 +1,36 @@
+// function fun (){
+
+//     console.log(this);
+// }
+
+// fun();
+
+// function func(){
+
+//     "use strict"
+//     this.name="arman";
+// };
+
+// func();
+
+
+// console.log(name);
+
+
+var Myobject = {
+  name: 'arman',
+  roll: 21,
+  AnotherObject: {
+    name:'amran',
+    value: function(){
+      console.log(this);
+    }
+  },
+
+  }
+
+  // Myobject.AnotherObject.value();
+  Myobject.AnotherObject.value.call(Myobject);
+
+
+// console.log(this);
