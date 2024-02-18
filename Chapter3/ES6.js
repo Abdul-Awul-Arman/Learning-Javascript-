970e31fa6c4b680b568a22a90084bee23980b3c2
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-02-18 10:14:58 +0600

    learn about tagged templates

---
+++ Chapter3/ES6.js
@@ -0,0 +1,25 @@
+// let name='arman';
+// let age=32;
+
+// let full=(`My name is ${name} and my age is ${age}`);
+
+// console.log(full);
+
+
+
+let fun = (string,name, age) => {
+    
+
+
+    console.log(string);
+    console.log(name);
+    console.log(age);
+};
+
+let name='arman';
+let age=30;
+
+
+
+fun`this is arman ${name}${age}`;
+
