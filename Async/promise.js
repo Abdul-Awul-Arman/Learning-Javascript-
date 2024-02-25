3a28ccb022e3ed38f9ef2212e97445b0fe93aa64
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-02-25 11:50:19 +0600

    learn more about multipul promise

--- Async/promise.js
+++ Async/promise.js
@@ -12,8 +12,25 @@
             }, 3000);
         })
     }
+
+    const aPromise2 = (control) =>{
+        return new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                if(control){
+                    resolve();
+                }
+                else{
+                    reject();
+                }
+                
+            }, 3000);
+        })
+    }
+
+    aPromise(true);
+    aPromise2(true); 
  
-    aPromise(false)
+    Promise.all(aPromise,aPromise2)
     .then(()=>{
         console.log('this is a successful promise');
     }).catch(()=>{
