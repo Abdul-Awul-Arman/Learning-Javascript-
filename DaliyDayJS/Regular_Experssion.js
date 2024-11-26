ac4305d9a8b940ea445886a7186be49a03d69a6a
AbdulAwulArman <abdulawularman52114@gmail.com>
2024-11-26 12:33:07 +0600

    learning regEx

---
+++ DaliyDayJS/Regular_Experssion.js
@@ -0,0 +1,20 @@
+// const aTex = "I am from mydesh. mydesh is beautiful";
+
+// // const regEx = /Mydesh/gi;
+
+// const regEx=/[^abc]/;
+
+// // const newText = aTex.replace(regEx, "bangladesh");
+
+// // console.log(newText);
+
+// const array = aTex.match(regEx);
+// console.log(array);
+
+    let number='01830014616';
+
+    const regEx=/[0-9]/g;
+
+    let array=number.match(regEx);
+
+    console.log(array);
